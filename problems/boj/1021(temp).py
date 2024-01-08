8484976bd7c0599e175a2b99111fa9599eaa4d3e
khlee <khlee@seowoninfo.com>
2024-01-08 11:00:36 +0900

    boj-1021

--- problems/boj/1021(temp).py
+++ problems/boj/1021(temp).py
@@ -3,25 +3,18 @@ from collections import deque
 N, M = map(int, input().split(' '))
 nums = list(map(int, input().split(' ')))
 d = deque(range(1, N + 1))
-cursor = d[0]
 ans = 0
 
 for n in nums:
-  if cursor == n:
-    d.popleft()
-    cursor = d[0]
-    continue
-  elif max(d) - n > cursor - n:
-    print(d, n, cursor, 'm')
-    while not cursor == n:
-      d.rotate(1)
-      ans += 1
-      cursor = d[0]
-  else:
-    print(d, n, cursor, 'mm')
-    while not cursor == n:
-      d.rotate(-1)
-      ans += 1
-      cursor = d[0]
-
+  while True:
+    if d[0] == n:
+      d.popleft()
+      break
+    else:
+      if len(d) / 2 > d.index(n):
+        d.append(d.popleft())
+        ans += 1
+      else:
+        d.appendleft(d.pop())
+        ans += 1
 print(ans)

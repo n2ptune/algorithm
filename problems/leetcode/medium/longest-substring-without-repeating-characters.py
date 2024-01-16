672ce304224ec9d5e5cdee1cc2e90b54e7aeed53
n2ptune <n2ptune@outlook.com>
2024-01-16 15:20:26 +0900

    leetcode-longest-substring-without-repeating-characters

---
+++ problems/leetcode/medium/longest-substring-without-repeating-characters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
+
+        if len(set(s)) == len(s):
+          return len(s)
+
+        su = ''
+        for ch in s:
+          if ch not in su:
+            su += ch
+            ans = max(ans, len(su))
+          else:
+            su = su.split(ch)[1] + ch
+
+print(Solution().lengthOfLongestSubstring('abcabcbb'))
